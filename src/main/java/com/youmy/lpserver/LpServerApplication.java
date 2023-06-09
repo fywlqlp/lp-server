43dfc0f735d6375d60dc3aa4c098e45d0449a2f8
fywlqlp <fywlq_lp@yeah.net>
2023-06-09 14:25:26 +0800

    first commit:

---
+++ src/main/java/com/youmy/lpserver/LpServerApplication.java
@@ -0,0 +1,13 @@
+package com.youmy.lpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LpServerApplication.class, args);
+    }
+
+}
